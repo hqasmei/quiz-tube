5268035eba88f1f63d87a15154a02b4333d7ee0f
Kanika Shah <kanika@trmlabs.com>
2024-05-29 11:45:35 -0700

    Updating README with steps to get dependency environment up and running

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -2,7 +2,7 @@ export default {
   providers: [
     {
       domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
-      applicationID: 'convex',
+      applicationID: 'convex', // This is hardcoded name coming from Clerk. This indicates that JWT token is created for "convex" application
     },
   ],
 };
